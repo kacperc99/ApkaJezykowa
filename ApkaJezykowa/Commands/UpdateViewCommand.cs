84b030b10ab3c8f6ef5f58d5b1582672ad8b2d96
kacperc99 <80091834+kacperc99@users.noreply.github.com>
2022-05-15 16:32:41 +0200

    Initial commit

---
+++ ApkaJezykowa/Commands/UpdateViewCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ApkaJezykowa.MVVM.ViewModel;
+
+namespace ApkaJezykowa.Commands
+{
+    internal class UpdateViewCommand : ICommand
+    {
+        private MainViewModel viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            Console.WriteLine("Clicked!");
+            if(parameter.ToString() == "Lessons")
+            {
+                viewModel.SelectedViewModel = new LessonsViewModel();
+            }
+            if (parameter.ToString() == "Dictionary")
+            {
+                viewModel.SelectedViewModel = new DictionaryViewModel();
+            }
+        }
+    }
+}
---
+++ ApkaJezykowa/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApkaJezykowa.Main;
+using System.Windows.Input;
+using ApkaJezykowa.Commands;
+
+namespace ApkaJezykowa.MVVM.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedViewModel;
+
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set { _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+
+            }
+        }
+        public ICommand UpdateViewCommand { get; set; }
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+    }
+}
