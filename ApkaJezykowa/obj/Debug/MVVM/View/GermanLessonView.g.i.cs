91a93ceb836c3237fb2bffa724e9a907a957d5cc
kacperc99 <80091834+kacperc99@users.noreply.github.com>
2022-11-19 02:06:00 +0100

    Dodatkowe ekrany
    no i na razie tyle

--- ApkaJezykowa/obj/Debug/MVVM/View/GermanLessonView.g.i.cs
+++ ApkaJezykowa/obj/Debug/MVVM/View/GermanLessonView.g.i.cs
@@ -40,6 +40,14 @@ namespace ApkaJezykowa.MVVM.View {
     /// </summary>
     public partial class GermanLessonView : System.Windows.Controls.UserControl, System.Windows.Markup.IComponentConnector {
         
+        
+        #line 35 "..\..\..\..\MVVM\View\GermanLessonView.xaml"
+        [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
+        internal System.Windows.Controls.Button GermanLessonReturn;
+        
+        #line default
+        #line hidden
+        
         private bool _contentLoaded;
         
         /// <summary>
@@ -68,6 +76,12 @@ namespace ApkaJezykowa.MVVM.View {
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1800:DoNotCastUnnecessarily")]
         void System.Windows.Markup.IComponentConnector.Connect(int connectionId, object target) {
+            switch (connectionId)
+            {
+            case 1:
+            this.GermanLessonReturn = ((System.Windows.Controls.Button)(target));
+            return;
+            }
             this._contentLoaded = true;
         }
     }
