96e87418188bef6c04e1895ab874d3b30fae1b7b
kacperc99 <80091834+kacperc99@users.noreply.github.com>
2023-01-08 14:07:22 +0100

    poprawkisf
    sdafas

--- ApkaJezykowa/MVVM/ViewModel/FrenchExerciseViewModel.cs
+++ ApkaJezykowa/MVVM/ViewModel/FrenchExerciseViewModel.cs
@@ -1,10 +1,12 @@
-﻿using ApkaJezykowa.MVVM.Model;
+﻿using ApkaJezykowa.Main;
+using ApkaJezykowa.MVVM.Model;
 using ApkaJezykowa.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ApkaJezykowa.MVVM.ViewModel
 {
@@ -12,26 +14,50 @@ namespace ApkaJezykowa.MVVM.ViewModel
   {
     string _test;
     public List<string> _exercise = new List<string>();
-    public List<string> _answer = new List<string>();
-    public List<string> _tip = new List<string>();
+    public List<string> _answer = new List<string>(new string[10]);
+    public List<string> _tip = new List<string>(new string[10]);
     private IExerciseRepository exerciseRepository;
 
     public string Test { get { return _test; } set { _test = value; OnPropertyChanged(nameof(Test)); } }
     public List<string> Exercise { get { return _exercise; } }
     public List<string> Answer { get { return _answer; } }
     public List<string> Tip { get { return _tip; } }
+    public ICommand CheckAnswers { get; }
     public FrenchExerciseViewModel()
     {
       exerciseRepository = new ExerciseRepository();
+      CheckAnswers = new RelayCommand(ExecuteCheckAnswers, CanExecuteCheckAnswers);
       LoadCurrentCourseLevel();
+    }
+
+    private bool CanExecuteCheckAnswers(object arg)
+    {
+      return true;
+    }
+
+    private void ExecuteCheckAnswers(object obj)
+    {
+      for(int i = 0; i < 10; i++)
+      {
+        if(Answer[i]==ExerciseModel.Instance.Answer[i])
+        {
+          Tip[i]="Prawidłowa odpowiedź!";
+          Console.WriteLine(Answer[i]);
+          Console.WriteLine(Tip[i]);
+        }
+        else
+        {
+          Tip[i]=ExerciseModel.Instance.Tip[i];
+          Console.WriteLine(Answer[i]);
+          Console.WriteLine(Tip[i]);
+        }
+      }
     }
 
     private void LoadCurrentCourseLevel()
     {
-      Console.WriteLine(ExerciseLevelModel.Instance.Level.ToString());
-      Console.WriteLine("Trzeci Paramentr");
-      Test = ExerciseModel.Instance.TaskText;
       exerciseRepository.Display(ExerciseLevelModel.Instance.Level, ExerciseLevelModel.Instance.Language);
+      Test = ExerciseModel.Instance.TaskText;
       Exercise.AddRange(ExerciseModel.Instance.Exercise);
     }
   }
