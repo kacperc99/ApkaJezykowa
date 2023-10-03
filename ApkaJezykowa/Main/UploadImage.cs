327573ce366970e049de4cf8371f4a164bfb94d1
kacperc99 <80091834+kacperc99@users.noreply.github.com>
2023-10-03 22:03:11 +0200

    p
    p

--- ApkaJezykowa/Main/UploadImage.cs
+++ ApkaJezykowa/Main/UploadImage.cs
@@ -9,5 +9,13 @@ namespace ApkaJezykowa.Main
 {
   internal class UploadImage
   {
+    public void ImageUploader()
+    {
+      var blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=languageappfiles;AccountKey=Lq2MOaQcGN9BZ5iJW7iScsNgYceKRLS5h/ox76xVe6Dl40ZHfCHLQfs7UZCZs65TYqRfwxs6hRC9+AStvMK0nQ==;EndpointSuffix=core.windows.net";
+      var blobStorageContainerName = "lessonimages";
+
+      var container = new BlobContainerClient(blobStorageConnectionString, blobStorageContainerName);
+      //BlobClient blob = container.GetBlobClient(blobName);
+    }
   }
 }
