5ac95088e10fbae28a76d92367d4bcab6ae55061
kacperc99 <80091834+kacperc99@users.noreply.github.com>
2023-10-23 00:23:06 +0200

    database update
    uh, have I updated the right app?

--- ApkaJezykowa/Repositories/BaseRepository.cs
+++ ApkaJezykowa/Repositories/BaseRepository.cs
@@ -12,10 +12,10 @@ namespace ApkaJezykowa.Repositories
     private readonly string _connectionString;
     public BaseRepository()
     {
-      //_connectionString = @"Server=DESKTOP-TJ02NPR\LINGUONATOR2; Database=userDB; Integrated Security=true";
-      _connectionString = @"Server=tcp:linguonator2.database.windows.net,1433;Initial Catalog=LINGUONATOR2;Persist Security Info=False;User ID=kc99;Password=EpIcPaSsWoRd!9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
-      
-      //_connectionString = @"Server = DESKTOP - LFET3QL\LINGUONATOR2; Database = userDB; Integrated Security = true";
+            //_connectionString = @"Server=DESKTOP-TJ02NPR\LINGUONATOR2; Database=userDB; Integrated Security=true";
+            //_connectionString = @"Server=tcp:linguonator2.database.windows.net,1433;Initial Catalog=LINGUONATOR2;Persist Security Info=False;User ID=kc99;Password=EpIcPaSsWoRd!9;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+            _connectionString = @"Server=tcp:192.168.50.116,1433,1433; Network Library=DBMSSOCN; Initial Catalog=userDB; User ID=app; Password=app;";
+      //_connectionString = @"Server=DESKTOP-LFET3LQ\LINGUONATOR2; Database=userDB; Integrated Security=true";
     }
     protected SqlConnection GetConnection()
     {
